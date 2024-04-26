1ac731502010320b08c961f3fc9c46c80611f24d
Renan Tomasi <rnantg21@gmail.com>
2024-04-26 19:13:11 -0300

    add routes

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/modulos')
+def modulos():
+    return render_template('modulos.html')
