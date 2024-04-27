e003beaf582df0588faf8cab8de06e2e009f8d85
Madhs31 <fernandinha.mf30@gmail.com>
2024-04-27 17:21:30 -0300

    Modificações

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -14,26 +14,26 @@ def modulos():
 def modulo_1():
     return render_template('mod_1.html')
 
-@app.route('/modulo_2')
+@app.route('/mod_2')
 def modulo_2():
     return render_template('mod_2.html')
 
-@app.route('/modulo_3')
+@app.route('/mod_3')
 def modulo_3():
     return render_template('mod_3.html')
 
-@app.route('/modulo_4')
+@app.route('/mod_4')
 def modulo_4():
     return render_template('mod_4.html')
 
-@app.route('/modulo_5')
+@app.route('/mod_5')
 def modulo_5():
     return render_template('mod_5.html')
 
-@app.route('/modulo_6')
+@app.route('/mod_6')
 def modulo_6():
     return render_template('mod_6.html')
 
-@app.route('/modulo_7')
+@app.route('/mod_7')
 def modulo_7():
     return render_template('mod_7.html')
