11f0213c84d14ff3b7b0fbe89fa00c95a3b1ec35
ilyraphaela <bmraphaela@gmail.com>
2024-05-03 09:24:36 -0300

    update módulo 7

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -285,9 +285,78 @@ def submit_quiz_6():  #altere apenas o número do seu módulo
     total_questions = len(questions_6)
     return render_template('mod_6_results.html', score=score, total_questions=total_questions, results=user_answers) #altere apenas o número do seu módulo no (mod_x_results.html)
 
-#Módulo 7
 
+#Módulo 7
 @app.route('/mod_7')
+def modulo_7():
+    return render_template('mod_7.html')
+
+#Módulo 7 - quiz
+
+questions_7 = [ #question_<seu módulo>
+    {
+        'id': 1, #id segue padrão para todas
+        'question': 'Qual é o principal objetivo do Planning Poker no SCRUM?', #coloque sua pergunta aqui
+        'options': ['Determinar quem são os membros mais rápidos da equipe', 'Estabelecer um consenso sobre o esforço necessário para as tarefas.', 'Aumentar a competição entre os membros da equipe', 'Acelerar o processo de desenvolvimento de software.', 'Identificar os membros da equipe que precisam de treinamento adicional.'], #opções de resposta
+        'answer': 'Estabelecer um consenso sobre o esforço necessário para as tarefas.' #insira a resposta certa 
+    },
+    {
+        'id': 2,
+        'question': 'O que a auto-organização promove nas equipes ágeis?',
+        'options': ['Conformidade', 'Individualismo', 'Colaboração', 'Resistência à mudança', 'Competição'],
+        'answer': 'Colaboração'
+    },
+    {
+        'id': 3,
+        'question': 'Qual é uma das responsabilidades do Scrum Master?',
+        'options': ['Promover o engajamento', 'Ditar as tarefas da equipe', 'Resolver todos os conflitos', 'Focar apenas nos resultados', 'Delegação excessiva'],
+        'answer': 'Promover o engajamento'
+    },
+    {
+        'id': 4,
+        'question': 'O que são User Stories em metodologias ágeis?',
+        'options': ['Grandes descrições de funcionalidades', 'Requisitos escritos apenas pelo Product Owner', ' Histórias irrelevantes', 'Documentos detalhados e rígidos', 'Pequenas descrições de funcionalidades'],
+        'answer': 'Pequenas descrições de funcionalidades'
+    },
+    {
+        'id': 5,
+        'question': 'Qual é um dos benefícios da auto-organização?',
+        'options': ['Microgerenciamento eficiente', 'Colaboração ativa', 'Resistência à mudança', 'Conformidade absoluta', ' Falta de autonomia'],
+        'answer': 'Promover o engajamento'
+    },
+    {
+        'id': 6,
+        'question': 'Qual é uma das funções do Scrum Master como facilitador?',
+        'options': ['Aumentar os conflitos na equipe', 'Minimizar a comunicação entre os membros', 'Ignorar as necessidades dos stakeholders', 'Otimizar os processos de trabalho', 'Incentivar a competição entre os membros'],
+        'answer': 'Otimizar os processos de trabalho'
+    },
+    {
+        'id': 7,
+        'question': 'O que é a visão do produto em metodologias ágeis?',
+        'options': ['Uma declaração clara dos métodos de desenvolvimento', 'Um documento rígido com requisitos detalhados', 'Uma declaração dos objetivos e valores do produto', ' Uma descrição dos recursos do produto', 'Um plano de projeto estático'],
+        'answer': 'Uma declaração dos objetivos e valores do produto'
+    },
+    {
+        'id': 8,
+        'question': 'Qual é a importância dos critérios de aceitação em User Stories?',
+        'options': ['Determinar o tamanho da história', 'Garantir que a história seja implementada', 'Avaliar o desempenho do Scrum Master', 'Controlar o escopo do projeto', 'Promover a inovação na equipe'],
+        'answer': 'Garantir que a história seja implementada'
+    },
+    {
+        'id': 9,
+        'question': 'Qual é um dos elementos principais de uma User Story?',
+        'options': ['Código-fonte', ' Estrutura de dados', 'Diagrama de Gantt', ' Lista de tarefas', ' Who, What, Why'],
+        'answer': ' Who, What, Why'
+    },
+    {
+        'id': 10,
+        'question': 'Qual é o papel do Product Owner em relação à visão do produto?',
+        'options': ['Criar a visão do produto sozinho', 'Desenvolver a visão do produto durante a Sprint Planning', 'Transmitir a visão do produto para a equipe', ' Ignorar a visão do produto após sua criação', 'Promover a competição entre os membros da equipe'],
+        'answer': 'Transmitir a visão do produto para a equipe'
+    },
+]
+
+@app.route('/mod_7') #seu módulo normal
 def modulo_7():
     return render_template('mod_7.html')
 
