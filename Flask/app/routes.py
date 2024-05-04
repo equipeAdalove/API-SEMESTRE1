1ad1ce599c7f7f815ae5d75f02cd9fff4c5d69dc
Vitor Ribeiro <vitorribeiro.inacio0904@gmail.com>
2024-05-04 15:32:07 -0300

    add routes

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -99,16 +99,98 @@ def submit_quiz_1():
         if user_answer == correct_answer:
             score += 1
         user_answers[question['question']] = user_answer
-    total_questions = len(questions_6)
+    total_questions = len(questions_1)
     return render_template('mod_1_results.html', score=score, total_questions=total_questions, results=user_answers)
 
 @app.route('/mod_2')
 def modulo_2():
     return render_template('mod_2.html')
 
+questions_3 = [
+    {
+        'id': 1,
+        'question': '1) Qual das seguintes tarefas é uma responsabilidade do Product Owner?',
+        'options': ['Codificar funcionalidades do produto.', ' Supervisionar o plano de lançamentos.', 'Executar testes de qualidade do produto.', 'Configurar o ambiente de desenvolvimento.'],
+        'answer': 'Supervisionar o plano de lançamentos.'
+    },
+    {
+        'id': 2,
+        'question': '2) Qual é uma das principais responsabilidades do Scrum Master?',
+        'options': ['Ditar regras para a equipe.', 'Ser o líder absoluto da equipe.', 'Garantir que a equipe siga as regras e valores do Scrum.', 'Tomar todas as decisões importantes no projeto.'],
+        'answer': 'Garantir que a equipe siga as regras e valores do Scrum.'
+    },
+    {
+        'id': 3,
+        'question': '3) Qual é a principal responsabilidade do Development Team em um framework Scrum?',
+        'options': ['Estimar o esforço necessário para cada item do Product Backlog.', 'Criar partes do produto que possam ser entregues ao final de cada Sprint.', 'Gerenciar o trabalho do Scrum Master.', 'Definir as metas da Sprint.'],
+        'answer': 'Criar partes do produto que possam ser entregues ao final de cada Sprint.Criar partes do produto que possam ser entregues ao final de cada Sprint.'
+    },
+    {
+        'id': 4,
+        'question': '4) Qual das seguintes afirmações melhor descreve uma característica das equipes Scrum?',
+        'options': ['As equipes Scrum têm posições fixas, onde cada membro desempenha sempre o mesmo papel em todos os projetos.', 'As equipes Scrum são autogerenciadas e possuem a capacidade de decidir como realizar o trabalho sem supervisão constante.', 'As equipes Scrum dependem fortemente de recursos externos para realizar suas tarefas, tornando-as menos ágeis.', 'As equipes Scrum são especializadas em uma única habilidade e não possuem versatilidade para realizar diferentes tarefas.'],
+        'answer': 'As equipes Scrum são autogerenciadas e possuem a capacidade de decidir como realizar o trabalho sem supervisão constante.'
+    },
+    {
+        'id': 5,
+        'question': '5) Por que é importante que o Product Owner defina a visão do produto?',
+        'options': ['Para garantir que todas as funcionalidades sejam implementadas.', 'Para comunicar claramente os requisitos aos desenvolvedores.',  'Para controlar o acesso ao Product Backlog.', 'Para decidir quando cancelar uma Sprint.'],
+        'answer': 'Para comunicar claramente os requisitos aos desenvolvedores.'
+    },
+    {
+        'id': 6,
+        'question': '6) Como o Scrum Master lidera a equipe?',
+        'options': ['Através do controle autoritário.', 'Estabelecendo regras rígidas.', 'Pelo exemplo e pelo conhecimento.', 'Ignorando as necessidades individuais dos membros da equipe.'],
+        'answer': 'Pelo exemplo e pelo conhecimento.'
+    },
+    {
+        'id': 7,
+        'question': '7) Por que é importante que os Development Teams sejam do tamanho certo?',
+        'options': ['Para maximizar o número de membros no time', 'Para facilitar a coordenação e colaboração.', 'Para aumentar a competitividade entre os membros.', ' Para permitir a intervenção externa mais eficaz.'],
+        'answer': 'Para facilitar a coordenação e colaboração.'
+    },
+    {
+        'id': 8,
+        'question': '8) Quem tem a autoridade para aceitar ou rejeitar o trabalho concluído no final de cada ciclo de trabalho?',
+        'options': [' O Scrum Master.', ' A equipe de desenvolvimento.', 'O Product Owner.', 'O cliente final.'],
+        'answer': 'O Product Owner.'
+    },
+    {
+        'id': 9,
+        'question': '9) Qual é o papel do Scrum Master durante as reuniões do Scrum?',
+        'options': ['Liderar todas as discussões.', 'Garantir que todos tenham a chance de falar e que as decisões sejam tomadas rapidamente.', ' Ignorar as contribuições dos membros da equipe.', '  Impor suas próprias opiniões e decisões aos outros.'],
+        'answer': 'Garantir que todos tenham a chance de falar e que as decisões sejam tomadas rapidamente.'
+    },
+    {
+        'id': 10,
+        'question': '10) Qual dos seguintes estágios é visto como parte do desenvolvimento de um time auto-organizado no Scrum?',
+        'options': ['Planejamento.', ' Execução.', ' Conflito.', 'Monitoramento.'],
+        'answer': 'Conflito.'
+    },
+]
+
 @app.route('/mod_3')
 def modulo_3():
     return render_template('mod_3.html')
+
+@app.route('/mod_3_quiz')
+def modulo_3_quiz():
+    return render_template('mod_3_quiz.html', questions=questions_3)
+
+@app.route('/submit_quiz_3', methods=['POST'])
+def submit_quiz_3():
+    # Lógica para processar o formulário e verificar as respostas
+    score = 0
+    user_answers = {}
+    for question in questions_3:
+        question_id = str(question['id'])
+        user_answer = request.form.get(question_id)
+        correct_answer = question['answer']
+        if user_answer == correct_answer:
+            score += 1
+        user_answers[question['question']] = user_answer
+    total_questions = len(questions_3)
+    return render_template('mod_3_results.html', score=score, total_questions=total_questions, results=user_answers)
 
 # Módulo 4
 
@@ -188,14 +270,14 @@ def submit_quiz_4():  #altere apenas o número do seu módulo
     # Lógica para processar o formulário e verificar as respostas
     score = 0
     user_answers = {}
-    for question in questions_6:
+    for question in questions_4:
         question_id = str(question['id'])
         user_answer = request.form.get(question_id)
         correct_answer = question['answer']
         if user_answer == correct_answer:
             score += 1
         user_answers[question['question']] = user_answer
-    total_questions = len(questions_6)
+    total_questions = len(questions_4)
     return render_template('mod_4_results.html', score=score, total_questions=total_questions, results=user_answers)
 
  # Módulo 5
