ab4f6e04177baaf756a34f6cd5e1c5545d214d44
Madhs31 <fernandinha.mf30@gmail.com>
2024-05-03 22:42:04 -0300

    Modificações

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -110,9 +110,79 @@ def modulo_2():
 def modulo_3():
     return render_template('mod_3.html')
 
+#Módulo 4
+
+questions_4 = [
+    {
+        'id': 1,
+        'question': 'Qual é a principal transformação que o desenvolvimento de software passou nos últimos anos?',
+        'options': ['Aumento da rigidez nas abordagens de gestão de projetos.', 'Maior foco na entrega incremental.', 'Redução da competitividade no mercado.', 'A adoção de métodos ágeis.'],
+        'answer': 'A adoção de métodos ágeis.'
+    },
+    {
+        'id': 2,
+        'question': 'Quais são os três pilares fundamentais do Scrum?',
+        'options': [' Planejamento, execução, controle', 'Transparência, inspeção, adaptação', 'Documentação, revisão, entrega', 'Individualidade, inovação, adaptação'],
+        'answer': 'Transparência, inspeção, adaptação'
+    },
+    {
+        'id': 3,
+        'question': 'Onde o Scrum pode ser aplicado?',
+        'options': ['Apenas em projetos de desenvolvimento de software', 'Apenas em projetos com requisitos estáveis e definidos', 'A uma variedade de projetos, desde que haja complexidade e incerteza', 'Apenas em empresas de grande porte'],
+        'answer': 'A uma variedade de projetos, desde que haja complexidade e incerteza'
+    },
+    {
+        'id': 4,
+        'question': 'Qual é um dos benefícios do Scrum em termos de gerenciamento de projetos?',
+        'options': ['Maior rigidez no controle do progresso do projeto', 'Redução da colaboração entre os membros da equipe', ' Detecção precoce de problemas', 'Menor foco no valor entregue ao cliente'],
+        'answer': 'Detecção precoce de problemas'
+    },
+    {
+        'id': 5,
+        'question': 'Quem são os criadores do SCRUM?',
+        'options': ['Steve Jobs e Bill Gates.', 'Jeff Sutherland e Ken Schwaber.', 'Tim Berners-Lee e Linus Torvalds.', 'Larry Page e Sergey Brin.'],
+        'answer': 'Jeff Sutherland e Ken Schwaber.'
+    },
+    {
+        'id': 6,
+        'question': 'O que é enfatizado pelo Manifesto Ágil?',
+        'options': ['Indivíduos e interações, software em funcionamento, colaboração com o cliente e resposta a mudanças', 'Processos e ferramentas, documentação abrangente, negociação de contratos, seguir um plano', 'Entrega de produtos finais sem interações com o cliente', 'Priorização de documentação sobre indivíduos e interações'],
+        'answer': 'Indivíduos e interações, software em funcionamento, colaboração com o cliente e resposta a mudanças'
+    },
+    {
+        'id': 7,
+        'question': 'Quais são alguns exemplos práticos de valores do Scrum?',
+        'options': ['Controle e estagnação', 'Rigidez e documentação abrangente', 'Foco e respeito', ' Falta de colaboração e coragem'],
+        'answer': 'Foco e respeito'
+    },
+    {
+        'id': 8,
+        'question': 'O que os princípios ágeis enfatizam?',
+        'options': ['Satisfação do desenvolvedor', 'Satisfação do cliente, entrega incremental, colaboração e adaptação contínua', 'Foco na documentação e nos processos', 'Resistência à mudança'],
+        'answer': 'Satisfação do cliente, entrega incremental, colaboração e adaptação contínua'
+    },
+    {
+        'id': 9,
+        'question': 'O que é essencial para promover a colaboração, o trabalho em equipe e a busca pela excelência no Scrum?',
+        'options': ['Rigidez e falta de adaptação', ' Resistência à mudança', 'Valores e princípios do Scrum', ' Controle centralizado'],
+        'answer': ' WValores e princípios do Scrum'
+    },
+    {
+        'id': 10,
+        'question': 'O que são Métodos Ágeis?',
+        'options': ['Abordagens inflexíveis e rígidas.', 'Estratégias para reduzir a colaboração da equipe.', 'Uma abordagem flexível e adaptativa para maximizar o valor entregue ao cliente.', ' Práticas para evitar feedback contínuo.'],
+        'answer': 'Uma abordagem flexível e adaptativa para maximizar o valor entregue ao cliente.'
+    },
+]
+
+
 @app.route('/mod_4')
 def modulo_4():
     return render_template('mod_4.html')
+
+@app.route('/mod_4_quiz')
+def modulo_1_quiz():
+    return render_template('mod_4_quiz.html', questions=questions_1)
 
  # Módulo 5
 
