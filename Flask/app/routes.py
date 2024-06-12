bbb0e66f0711ff1aeceab9247539f5ef17964fd4
Celso <celsorrochajr2005@gmail.com>
2024-06-12 10:30:13 -0300

    routes and scripts update

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -1,6 +1,29 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
 
+# Configuração do MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fatec'
+app.config['MYSQL_DB'] = 'api1ads'
+mysql = MySQL(app)
+
+# Rota para adicionar uma nova tarefa
+@app.route('/add', methods=['POST'])
+def add_task():
+    if request.method == 'POST':
+        id = request.form['id']
+        nome = request.form['nome']
+        nota = request.form['nota']
+        opiniao = request.form['opiniao']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO formulario (id, nome, nota, opiniao) VALUES (%d, %s, %d, %s)", (id, nome, nota, opiniao))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('index'))
+
+# NavBar
 @app.route('/')
 @app.route('/index')
 def index():
